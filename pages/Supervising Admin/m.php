1e544ef41beaa3613f49ab64c375fc1eeed536a2
Mark-Abayan <129719889+Mark-Abayan@users.noreply.github.com>
2023-05-09 23:14:06 +0100

    v1.5.16
    fpdf

--- pages/Supervising Admin/m.php
+++ pages/Supervising Admin/m.php
@@ -3,45 +3,115 @@
 require('fpdf185/fpdf.php');
 
 
-// New object created and constructor invoked
-$pdf = new FPDF();
-  
-// Add new pages. By default no pages available.
-$pdf->AddPage();
-  
-// Set font format and font-size
-$pdf->SetFont('Times', 'B', 20);
-  
-// Framed rectangular area
-$pdf->Cell(176, 5, 'Welcome to GeeksforGeeks!', 0, 0, 'C');
+$pdf = new FPDF(); // New object created and constructor invoked
+$pdf->AddPage(); // Add new pages. By default no pages available.
+
+//Header//
+$pdf->SetFont('Times', '', 10);// Set font format and font-size
+$pdf->Cell(200, 10, 'Republic of the Philippines', 0, 0, 'C');// Framed rectangular area
  
-$imagenurl = "../login form/images/NMIS_LOGO.png"; // in my case
-// 1) left corner in coord x=1 and y=1
-$pdf->Cell(0, 0, $pdf->Image($imagenurl, 1,1,20,10), 0, 0, 'C', false,'');
+$pdf->Ln();// Set it new line
+$pdf->SetFont('Times', '', 10);// Set font format and font-size
+$pdf->Cell(200, -1, 'DEPARTMENT OF AGRICULTURE', 0, 0, 'C');// Framed rectangular area
 
-// 2) right corner in coord x=$pdf->GetX() - image width + 1 and y = $pdf->GetY() - image height + 1
-$pdf->Cell(0, 0, $pdf->Image($imagenurl, $pdf->GetX()-20+1,$pdf->GetY()-10+1,20,10), 0, 0, 'C', false,'');
-// Set it new line
+$pdf->Ln();// Set it new line
+$pdf->SetFont('Times', 'B', 10);// Set font format and font-size
+$pdf->Cell(200, 10, 'NATIONAL MEAT INSPECTION SERVICE', 0, 0, 'C');// Framed rectangular area
+
+$pdf->Ln();// Set it new line
+$pdf->SetFont('Times', 'B', 10);// Set font format and font-size
+$pdf->Cell(200, -1, 'No.4 Visayas Avenue, Brgy. Vasra, Quezon City', 0, 0, 'C');// Framed rectangular area
+
+$pdf->Ln();// Set it new line
+$pdf->SetFont('Times', '', 10);// Set font format and font-size
+$pdf->Cell(200, 10, 'Tel. N o. 8-924-7980', 0, 0, 'C');// Framed rectangular area
+
+$pdf->Ln();// Set it new line
+$pdf->SetFont('Times', '', 10);// Set font format and font-size
+$pdf->Cell(200, -1, 'www.nmis.gov.ph | nmis@nmis.gov.ph', 0, 0, 'C');// Framed rectangular area
+//end of the header//
+
+//A. To be filled-up by the Dispatcher //
+$pdf->Ln(5);// Set it new line
+$pdf->SetFont('Times', 'B', 9);// Set font format and font-size
+$pdf->Cell(60, 10, 'A. To be filled-up by the Dispatcher', 0, 0, 'C');// Framed rectangular area
+
+$pdf->Ln();// Set it new line
+$pdf->SetFont('Times', 'B', 9);// Set font format and font-size
+$pdf->Cell(29, 1, 'Control No.', 0, 0, 'C');// Framed rectangular area
+
+$pdf->Ln();// Set it new line
+$pdf->SetFont('Times', '', 9);// Set font format and font-size
+$pdf->Cell(42, 10, '1. Name of the Driver:', 0, 0, 'C');// Framed rectangular area
+
+$pdf->Ln();// Set it new line
+$pdf->SetFont('Times', '', 9);// Set font format and font-size
+$pdf->Cell(40, -1, '2. Vehicle Plate No.:', 0, 0, 'C');// Framed rectangular area
+
+$pdf->Ln();// Set it new line
+$pdf->SetFont('Times', '', 9);// Set font format and font-size
+$pdf->Cell(59, 10, '3. Name of Authorized Passenger/s:', 0, 0, 'C');// Framed rectangular area
+
+$pdf->Ln();// Set it new line
+$pdf->SetFont('Times', '', 9);// Set font format and font-size
+$pdf->Cell(53, -1, '4. Duration of Dates of Travel:', 0, 0, 'C');// Framed rectangular area
+
+$pdf->Ln();// Set it new line
+$pdf->SetFont('Times', '', 9);// Set font format and font-size
+$pdf->Cell(38, 10, '5. Fuel (Volume):  ', 0, 0, 'C');// Framed rectangular area
+
+$pdf->Ln();// Set it new line
+$pdf->SetFont('Times', '', 9);// Set font format and font-size
+$pdf->Cell(28, -1, '6. Purpose:', 0, 0, 'C');// Framed rectangular area
+//end //
+
+//tables//
+$pdf->Ln(5);// Set it new line
+$pdf->SetFont('Times','B', 9);
+$pdf->Cell(190,5,'ITINERARY',1,0,'C',0);
 $pdf->Ln();
-  
-// Set font format and font-size
-$pdf->SetFont('Times', 'B', 12);
-  
-// Framed rectangular area
-$pdf->Cell(176, 10, 'A Computer Science Portal for geek!', 0, 0, 'C');
+$pdf->SetFont('Times','', 9);
+$pdf->Cell(63,5,'Destination/s',1,0,'C',0);
+$pdf->Cell(64,5,'ETD (Time/ Date)',1,0,'C',0);
+$pdf->Cell(63,5,'ETA (Time/ Date)','LBR',1,'C',0);   
+$pdf->Cell(63,5,'',1,0,'C',0);
+$pdf->Cell(64,5,'',1,0,'C',0);
+$pdf->Cell(63,5,'','LBR',1,'C',0);  
+// end tables//
 
-// Set it new line
 $pdf->Ln();
+$pdf->SetFont('Times', '', 9);// Set font format and font-size
+$pdf->Cell(28, -1, 'Requested by:', 0, 0, 'C');// Framed rectangular area
+$pdf->Cell(200, -1, 'Approved by:', 0, 0, 'C');// Framed rectangular area
 
-$pdf->Cell(40,5,' ','LTR',0,'L',0);   // empty cell with left,top, and right borders
-$pdf->Cell(50,5,'Words Here',1,0,'L',0);
-$pdf->Cell(50,5,'Words Here',1,0,'L',0);
-$pdf->Cell(40,5,'Words Here','LR',1,'C',0);  // cell with left and right borders
-$pdf->Cell(50,5,'[ x ] abc',1,0,'L',0);
-$pdf->Cell(50,5,'[ x ] checkbox1',1,0,'L',0);
-$pdf->Cell(40,5,'','LBR',1,'L',0);   // empty cell with left,bottom, and right borders
-$pdf->Cell(50,5,'[ x ] def',1,0,'L',0);
-$pdf->Cell(50,5,'[ x ] checkbox2',1,0,'L',0);
+$pdf->Ln(5);// Set it new line
+$pdf->SetFont('Times', 'B', 9);// Set font format and font-size
+$pdf->Cell(29, 10, 'DR. YVETTE', 0, 0, 'C');// Framed rectangular area
+
+$pdf->SetFont('Times', 'B', 9);// Set font format and font-size
+$pdf->Cell(237, 10, 'LAMBERTO B. DELA CRUZ, JR. MPM', 0, 0, 'C');// Framed rectangular area
+
+$pdf->Ln();// Set it new line
+$pdf->SetFont('Times', 'I', 9);// Set font format and font-size
+$pdf->Cell(34, -1, 'Supervising Admin', 0, 0, 'C');// Framed rectangular area
+
+$pdf->SetFont('Times', 'I', 9);// Set font format and font-size
+$pdf->Cell(226, -1, 'Chief Administrative and Management Div.', 0, 0, 'C');// Framed rectangular area
+
+
+
+
+
+// $pdf->Ln();// Set it new line
+// $pdf->Cell(40,5,' ','LTR',0,'L',0);   // empty cell with left,top, and right borders
+// $pdf->Cell(50,5,'Words Here',1,0,'L',0);
+// $pdf->Cell(50,5,'Words Here',1,0,'L',0);
+// $pdf->Cell(40,5,'Words Here','LR',1,'C',0);  // cell with left and right borders
+// $pdf->Cell(50,5,'[ x ] abc',1,0,'L',0);
+// $pdf->Cell(50,5,'[ x ] checkbox1',1,0,'L',0);
+// $pdf->Cell(40,5,'','LBR',1,'L',0);   // empty cell with left,bottom, and right borders
+// $pdf->Cell(50,5,'[ x ] def',1,0,'L',0);
+// $pdf->Cell(50,5,'[ x ] checkbox2',1,0,'L',0);
 
   
   
